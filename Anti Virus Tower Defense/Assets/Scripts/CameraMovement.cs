2c9ae59060f8729fe8422798a18c9a268f16fa8b
lustephen <lustephen@ufl.edu>
2018-02-20 01:02:13 -0500

    Unity to Github
    Added Unity Project To Github.
    Tile System already created

---
+++ Anti Virus Tower Defense/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]    //Can acces from inspector 
+    private float cameraSpeed = 0;
+
+    private float xMax;
+    private float yMin;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+
+
+        GetInput();
+                
+	}
+
+    private void GetInput() //Moving Camera Using WASD
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+        }
+
+        //Sets Limit on Camera Movement
+       // transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0, xMax), Mathf.Clamp(transform.position.y, yMin, 0), -10);
+
+    }
+
+    public void SetLimits(Vector3 maxTile)    //Prevent Camera From Moving Offscreen
+    {
+        Vector3 wp = Camera.main.ViewportToWorldPoint(new Vector3(1, 0));   //Location of Bottom Right Coordinate of Camera
+
+        xMax = maxTile.x - wp.x;    //Max movement on x axis
+        yMin = maxTile.y - wp.y;    //Min movement on y axis
+    }
+}
---
+++ Anti Virus Tower Defense/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    [SerializeField]    //Can acces from inspector
+    private GameObject[] tilePrefabs;
+
+    [SerializeField]
+    private CameraMovement cameraMovement;
+
+    public float TileSize
+    {
+       get
+        {
+            return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        //Execute Map Creation
+        CreateLevel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void CreateLevel()
+    {
+        string[] mapData = ReadLevelText();
+
+        int mapX = mapData[0].ToCharArray().Length; //Length of each element in mapData
+        int mapY = mapData.Length;  //Length of mapData    
+
+
+        Vector3 maxTile = Vector3.zero;
+
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height)); //Coordinate of Top Left Corner of Camera/Screen
+        for (int y = 0; y < mapY; y++) //Y 
+        {
+            char[] newTiles = mapData[y].ToCharArray();
+
+
+            for (int x = 0; x < mapX; x++) //X 
+            {
+                maxTile = PlaceTile(newTiles[x].ToString(), x, y, worldStart);    //Places Tiles Accordingly to Level.txt
+         
+            }
+        }
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
+    }
+
+    private Vector3 PlaceTile(string tileType, int x, int y, Vector3 worldStart)   //Places Tiles Accordingly to Level.txt
+    {
+        int tileIndex = int.Parse(tileType);    //Pass tiletype:string to tileIndex:int
+        TileScript newTile = Instantiate(tilePrefabs[tileIndex]).GetComponent<TileScript>();   //New Object Tile
+
+        //Places tile According Right/Left, Top/Down
+
+        newTile.Setup(new Point(x, y), new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0));
+        return newTile.transform.position;  //Returns Value for maxTile
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-');     //Splits text document when reading "-"
+    }
+
+}
